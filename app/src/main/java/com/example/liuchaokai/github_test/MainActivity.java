d94e98d8078f4950342a1d6bd773bd1ef691fd0d
smielchk <1209354383@qq.com>
2017-09-21 09:53:11 +0800

    Add an demo project

---
+++ app/src/main/java/com/example/liuchaokai/github_test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.liuchaokai.github_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    void aaaa(){}
+    //ajgl ag
+}
